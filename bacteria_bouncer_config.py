6601aaebceb11eba5f41e4af013b5cbdf7b8b8c9
Alan Dong <alan.dong2007@gmail.com>
2026-01-31 01:30:45 -0500

    Release v1.0

---
+++ bacteria_bouncer_config.py
@@ -0,0 +1,14 @@
+#thresholding sensitivity
+std_dev_multiplier = 2
+safety_buffer = 3000
+
+#geometry and cropping
+border_buffer = 1
+crop_radius_ratio = 1.5
+
+#blob filtering
+pixel_floor = 15
+pixel_max_ratio = 0.15
+
+#blur settings
+gaussian_kernel_size = 1 
